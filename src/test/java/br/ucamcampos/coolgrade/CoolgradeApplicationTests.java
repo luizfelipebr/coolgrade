17ea8fac3ccf05c0927006e8c2f8a825faac0fcb
Luiz Felipe Rangel <luizfelipebr@gmail.com>
2020-08-17 14:47:41 -0300

    Projeto criado

---
+++ src/test/java/br/ucamcampos/coolgrade/CoolgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucamcampos.coolgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
