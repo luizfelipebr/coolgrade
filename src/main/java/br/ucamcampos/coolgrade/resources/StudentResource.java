369f914b3cb0dfafb00748cc4cdf633793b21c4c
Luiz Felipe Rangel <luizfelipebr@gmail.com>
2020-08-24 03:04:02 -0300

    CORS configurado

--- src/main/java/br/ucamcampos/coolgrade/resources/StudentResource.java
+++ src/main/java/br/ucamcampos/coolgrade/resources/StudentResource.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity.BodyBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ import br.ucamcampos.coolgrade.services.StudentService;
 
 @RestController
 @RequestMapping(value = "/students")
+@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "Location")
 public class StudentResource {
 
 	@Autowired
@@ -39,19 +42,19 @@ public class StudentResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody StudentNewDTO objDto) {
+	public ResponseEntity<Student> insert(@Valid @RequestBody StudentNewDTO objDto) {
 		Student obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody StudentDTO objDto, @PathVariable Integer id) {
+	public ResponseEntity<Student> update(@Valid @RequestBody StudentDTO objDto, @PathVariable Integer id) {
 		Student obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok().body(obj);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
